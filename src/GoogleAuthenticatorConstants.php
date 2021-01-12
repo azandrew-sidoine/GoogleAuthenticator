d7c6e5617433e9a70a82c8e21c44de0d4812abed
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2021-01-12 18:22:57 +0000

    Add GoogleAuthenticatorConstants class

---
+++ src/GoogleAuthenticatorConstants.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\GoogleAuthenticator;
+
+final class GoogleAuthenticatorConstants
+{
+
+    /**
+     * List of supported hashing algorithms
+     */
+    const SUPPORTE_ALG = ['SHA1', 'SHA256', 'SHA512'];
+}
